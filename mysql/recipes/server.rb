182c1765a93abba63ffd34d562c6877cb6de959e
Marius Ducea <marius.ducea@gmail.com>
2011-12-29 17:28:02 -0800

    updating mysql/postfix/memcached cookbooks to latest versions

--- mysql/recipes/server.rb
+++ mysql/recipes/server.rb
@@ -62,7 +62,7 @@ package "mysql-server" do
 end
 
 service "mysql" do
-  service_name value_for_platform([ "centos", "redhat", "suse", "fedora" ] => {"default" => "mysqld"}, "default" => "mysql")
+  service_name value_for_platform([ "centos", "redhat", "suse", "fedora", "scientific", "amazon" ] => {"default" => "mysqld"}, "default" => "mysql")
   if (platform?("ubuntu") && node.platform_version.to_f >= 10.04)
     restart_command "restart mysql"
     stop_command "stop mysql"
@@ -72,12 +72,22 @@ service "mysql" do
   action :nothing
 end
 
+skip_federated = case node['platform']
+                 when 'fedora', 'ubuntu', 'amazon'
+                   true
+                 when 'centos', 'redhat', 'scientific'
+                   node['platform_version'].to_f < 6.0
+                 else
+                   false
+                 end
+
 template "#{node['mysql']['conf_dir']}/my.cnf" do
   source "my.cnf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :restart, resources(:service => "mysql"), :immediately
+  variables :skip_federated => skip_federated
 end
 
 unless Chef::Config[:solo]
@@ -117,7 +127,7 @@ rescue
 end
 
 execute "mysql-install-privileges" do
-  command "/usr/bin/mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }#{node['mysql']['server_root_password']} < #{grants_path}"
+  command "/usr/bin/mysql -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < #{grants_path}"
   action :nothing
   subscribes :run, resources("template[#{grants_path}]"), :immediately
 end
